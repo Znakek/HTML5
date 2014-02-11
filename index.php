77b448e932ac6ea4f6cc95b434a5d6afaacf895a
Kévin Hartmann <Znakek@me.com>
2014-02-11 09:41:50 +0100

    Revert fde3f65..8c15368
    This rolls back to commit fde3f65e4d29d1755784d5625808ce5a09bddaff.

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
-<!DOCTYPE html >
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<title>video</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" type="text/css" href="css/main.css"/>
+<title>Untitled Document</title>
 </head>
 
 <body>
@@ -10,6 +11,5 @@
 	<div id="content">
 		Så er er vi kørende
 	</div>
-
 </body>
 </html>
