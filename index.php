78ff8c7f268ae8635fcbcf90a1f3f5a7eb4c5ac3
Kévin Hartmann <Znakek@me.com>
2014-02-11 09:39:00 +0100

    b

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html >
+<html >
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>video</title>
 <link rel="stylesheet" type="text/css" href="css/main.css"/>
-<title>Untitled Document</title>
 </head>
 
 <body>
@@ -11,5 +10,6 @@
 	<div id="content">
 		Så er er vi kørende
 	</div>
+
 </body>
 </html>
